a7659fab982487e3b07ad48d34698219a3bf6829
Sanyam0205 <sanyamsingh0205@gmail.com>
2024-12-04 16:16:36 +0530

    profile

--- express_server/login.js
+++ express_server/login.js
@@ -16,6 +16,16 @@ app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const corsOptions = {
+  origin: 'http://localhost:3000', // Your front-end origin
+  credentials: true, // Allow cookies and credentials
+};
+
+// Enable CORS for all routes
+app.use(cors(corsOptions));
+
+// Explicitly handle OPTIONS requests for preflight
+app.options('*', cors(corsOptions)); // Handle preflight requests
 app.use(
   session({
     secret: 'mycookiesaremycookiesnotyourcookiesbecausethesecookiesshallbemycookiesonly',
